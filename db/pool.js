c53bb6664e410d832fc3b00dd76a247946f2af14
Strallia Chao <134454347+strallia@users.noreply.github.com>
2024-08-08 17:17:27 -0700

    feat: Add error handler for postgres pool

--- db/pool.js
+++ db/pool.js
@@ -1,5 +1,11 @@
 const { Pool } = require("pg");
 
-module.exports = new Pool({
+const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
 });
+
+pool.on("error", (err, client) => {
+  console.error("Unexpected error on idle postgres client", err);
+});
+
+module.exports = pool;
